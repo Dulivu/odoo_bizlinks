52eaf821e4f0d05121d2ce9459a86abcb4a0cf1a
Jorge Quico C <GeorgeL1102@gmail.com>
2021-01-04 09:37:47 -0500

    Update __manifest__.py

--- cv_bizlink_despatch/__manifest__.py
+++ cv_bizlink_despatch/__manifest__.py
@@ -10,7 +10,7 @@ Consume webservices de Bizlinks
     """,
     'auto_install': False,
     'category': 'tools',
-    'depends' : ['stock','cv_catalogo_sunat','cv_bizlink'],
+    'depends' : ['stock','cv_catalogo_sunat','cv_catalogo_sunat_despatch','cv_bizlink'],
     'data': [
         'views/stock_move.xml'
     ],
